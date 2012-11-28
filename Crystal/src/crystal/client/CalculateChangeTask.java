5b88471f404953887dbf5adc4d7b2065a3332ada
aurora <venet1035@gmail.com>
2012-11-28 00:53:57 -0500

    Input: two dir Output: list of changed files and their contents

--- Crystal/src/crystal/client/CalculateChangeTask.java
+++ Crystal/src/crystal/client/CalculateChangeTask.java
@@ -1,91 +1,114 @@
 package crystal.client;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 import crystal.util.RunIt;
 import crystal.util.RunIt.Output;
+import difflib.DiffRow;
+import difflib.DiffRowGenerator;
 
 public class CalculateChangeTask {
 
-	public static class changeItem {
-		String _commitDate;
-		String _committer;
-		String _commitID;
+	public static class ChangeItem {
 		ArrayList<String> changedFiles;
 		ArrayList<String> changedFilesContents;
 
-		changeItem(String commitID, String commitDate, String committer) {
-			_commitID = commitID;
-			_commitDate = commitDate;
-			_committer = committer;
+		ChangeItem() {
 			changedFiles = new ArrayList<String>();
 			changedFilesContents = new ArrayList<String>();
 		}
 	}
 
-	public static ArrayList<changeItem> performTask(
-			ProjectPreferences _prefs) throws IOException {
-
-		Output exeResult;
-		ClientPreferences myClientPrefs = _prefs.getClientPreferences();
-
-		String[] gitAccountArgs = { "config", "user.name", };
-		exeResult = RunIt.execute(myClientPrefs.getGitPath(), gitAccountArgs,
-				myClientPrefs.getTempDirectory(), false);
-		String localAcc = exeResult.getOutput();
-
-		// git log --committer=aurora -1 --pretty=format:%cD
-		String[] gitCommitterArgs = { "log", "--committer=" + localAcc, "-1",
-				"--pretty=format:%ci" };
-		exeResult = RunIt.execute(myClientPrefs.getGitPath(), gitCommitterArgs,
-				myClientPrefs.getTempDirectory(), false);
-		String sinceCommitDate = exeResult.getOutput();
-
-		String[] gitFilesArgs = { "log", "--since", sinceCommitDate,
-				"--pretty=format:\"commit:%H;%ci;%cn\"", "-p" };
-		exeResult = RunIt.execute(myClientPrefs.getGitPath(), gitFilesArgs,
-				myClientPrefs.getTempDirectory(), false);
-
-		ArrayList<changeItem> longList = new ArrayList<changeItem>();
-
-		if (!exeResult.getOutput().equals("")) {
-
-			changeItem oneCommit;
-			String[] stCommit = exeResult.getOutput().split("commit:");
-
-			for (String oneCommitStr : stCommit) {
-				if (oneCommitStr.equals(""))
-					continue;
-
-				String[] oneCommitLines = oneCommitStr.split("\n");
-				
-				String[] headLine = oneCommitLines[0].split(";");
-				oneCommit = new changeItem(headLine[0],
-						headLine[1], headLine[2]);
-				
-				for (int i = 1; i < oneCommitLines.length; i++) {
-
-					if (oneCommitLines[i].startsWith("+++ b")) {
-						oneCommit.changedFiles.add(oneCommitLines[i]
-								.substring(5));
-						
-						String tmpChangeFileString = "";
-						
-						int k;
-						for (k = i + 1; k < oneCommitLines.length; k++)
-							tmpChangeFileString = tmpChangeFileString
-									+ oneCommitLines[k] + "\n";
-						
-						oneCommit.changedFilesContents.add(tmpChangeFileString);
-						System.out.println(tmpChangeFileString);
-						i = k-1;
-					}
-				}
-				longList.add(oneCommit);
+	private static List<String> fileToLines(String filename) {
+		List<String> lines = new ArrayList<String>();
+		String line = "";
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(filename));
+			while ((line = in.readLine()) != null) {
+				lines.add(line);
+			}
+		} catch (IOException e) {
+			// File not found
+			File f = new File(filename);
+			try {
+				f.createNewFile();
+			} catch (IOException e1) {
+				return lines;
 			}
 		}
-		return longList;
+		return lines;
+
+	}
+
+	public static ChangeItem performTask(String _gitPath, String myDir,
+			String yourDir) throws IOException {
+
+		// ChangedFilesSet is a union set for storing all changed files
+		// Files are uniquely existing in the set
+		Output exeResult;
+		String[] ChangedFiles;
+		HashSet<Object> ChangedFilesSet = new HashSet<Object>();
+
+		// Prepare the command
+		String[] gitFilesArgs = { "log", "-1", "--pretty=format:\"\"",
+				"--name-only" };
+
+		// Execute command in myDir
+		exeResult = RunIt.execute(_gitPath, gitFilesArgs, myDir, false);
+		ChangedFiles = exeResult.getOutput().split("\n");
+		for (String s : ChangedFiles)
+			ChangedFilesSet.add(s);
+
+		// Execute command in yourDir
+		exeResult = RunIt.execute(_gitPath, gitFilesArgs, myDir, false);
+		ChangedFiles = exeResult.getOutput().split("\n");
+		for (String s : ChangedFiles)
+			ChangedFilesSet.add(s);
+
+		ChangeItem files = new ChangeItem();
+
+		if (!myDir.endsWith(File.separator))
+			myDir = myDir + File.separator;
+
+		if (!yourDir.endsWith(File.separator))
+			yourDir = yourDir + File.separator;
+
+		int i = 1;
+		for (Object s : ChangedFilesSet) {
+
+			List<String> myFileLines = fileToLines(myDir + s);
+			List<String> urFileLines = fileToLines(yourDir + s);
+
+			DiffRowGenerator.Builder builder = new DiffRowGenerator.Builder();
+			builder.showInlineDiffs(false);
+			DiffRowGenerator dfg = builder.build();
+			List<DiffRow> rows = dfg.generateDiffRows(myFileLines, urFileLines);
+
+			String contentLines = "";
+			for (DiffRow dr : rows) {
+				if (dr.getTag() == DiffRow.Tag.DELETE)
+//					System.out.println("-\t" + dr.getOldLine());
+					contentLines = contentLines + "-\t"+dr.getOldLine()+"\n";
+				else if (dr.getTag() == DiffRow.Tag.INSERT)
+//					System.out.println("+\t" + dr.getNewLine());
+					contentLines = contentLines + "+\t"+dr.getNewLine()+"\n";
+				else if (dr.getTag() == DiffRow.Tag.CHANGE){
+//					System.out.println("+-\t" + dr.getNewLine());
+					contentLines = contentLines + "+\t"+dr.getNewLine()+"\n";
+					contentLines = contentLines + "-\t"+dr.getOldLine()+"\n";
+				}
+			}
+			files.changedFiles.add(i, (String) s);
+			files.changedFilesContents.add(i, contentLines);
+			i++;
+		}
+		return files;
 	}
 
 }
